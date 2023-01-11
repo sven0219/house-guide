8cfd114d3c1b914e6feaf4700dc40c758be69e0b
sven <sven.shen@wiredcraft.com>
2023-01-11 15:39:35 +0800

    First commit: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  base: '/ning-house-guide/',
+  title: '南京买房记录',
+  description: '南京买房记录',
+  themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: 'Link', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/sven0219' },
+                    { text: 'Blog',  link: 'https://www.svenshen.com' }
+                ]
+            }
+        ]
+    }
+}
