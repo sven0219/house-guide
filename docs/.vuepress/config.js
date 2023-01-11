5b0b64818ec67169cc2499740d0f2cbadff440a0
sven <sven.shen@wiredcraft.com>
2023-01-11 16:45:01 +0800

    Add some page item

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,13 +5,32 @@ module.exports = {
   themeConfig: {
         nav: [
             { text: '首页', link: '/' },
-            { 
-                text: 'Link', 
+            {
+                text: 'Link',
                 items: [
                     { text: 'Github', link: 'https://github.com/sven0219' },
                     { text: 'Blog',  link: 'https://www.svenshen.com' }
                 ]
             }
-        ]
+        ],
+   sidebar: [
+            {
+                title: '南京买房记录',
+                path: '/',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "关于", path: "/" }
+                ]
+            },
+            {
+              title: "目录",
+              path: '/playbook',
+              collapsable: false, // 不折叠
+              children: [
+                { title: "计划", path: "/playbook/plan" },
+                { title: "价格", path: "/playbook/price" }
+              ],
+            }
+          ]
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,5 @@
-# Hello VuePress
+# 本仓库
+
+计划2023年南京买房，因此创建此仓库用来记录
+
+url: https://house.svenshen.com
