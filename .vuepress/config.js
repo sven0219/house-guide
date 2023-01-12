b6d956630f50cf71591cb3fcd634502748b8b084
sven <sven.shen@wiredcraft.com>
2023-01-12 13:00:16 +0800

    添加南京政策法规页面

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,87 +1,92 @@
 module.exports = {
-  base: '/',
-  title: '买房调研',
-  description: '买房记录',
-  themeConfig: {
-      lastUpdated: '最后更新于',
-      docsRepo: 'sven0219/house-guide',
-      docsBranch: 'main',
-      editLinks: true,
-      editLinkText: '欢迎指正',
-      nav: [{
-          text: '首页',
-          link: '/'
-      },
-      {
-          text: 'Link',
-          items: [{
-              text: 'Github',
-              link: 'https://github.com/sven0219'
-          },
-          {
-              text: 'Blog',
-              link: 'https://www.svenshen.com'
-          }]
-      }],
-      sidebar: [{
-          title: '买房记录',
-          path: '/',
-          collapsable: false, // 不折叠
-          children: [{
-              title: "关于",
-              path: "/"
-          }]
-      },
-      {
-          title: "南京",
-          path: '/nanjing/why',
-          collapsable: true, // 折叠
-          children: [{
-              title: "计划",
-              path: "/nanjing/plan"
-          },
-          {
-              title: "价格",
-              path: "/nanjing/price"
-          }],
-      },
-      {
-          title: "芜湖",
-          path: '/wuhu/about',
-          collapsable: true, // 折叠
-          children: [{
-              title: "区域",
-              path: "/wuhu/area"
-          },
-          {
-              title: "政策",
-              path: "/wuhu/policy"
-          },
-          {
-              title: "学区",
-              path: "/wuhu/school"
-          },
-          {
-              title: "计划",
-              path: "/wuhu/plan"
-          },
-          {
-              title: "价格",
-              path: "/wuhu/price"
-          }],
-      },
-      {
-          title: "合肥",
-          path: '/hefei/why',
-          collapsable: true, // 折叠
-          children: [{
-              title: "计划",
-              path: "/hefei/plan"
-          },
-          {
-              title: "价格",
-              path: "/hefei/price"
-          }],
-      }]
-  }
+    base: '/',
+    title: '买房调研',
+    description: '买房记录',
+    themeConfig: {
+        lastUpdated: '最后更新于',
+        docsRepo: 'sven0219/house-guide',
+        docsBranch: 'main',
+        editLinks: true,
+        editLinkText: '欢迎指正',
+        nav: [{
+            text: '首页',
+            link: '/'
+        },
+        {
+            text: 'Link',
+            items: [{
+                text: 'Github',
+                link: 'https://github.com/sven0219'
+            },
+            {
+                text: 'Blog',
+                link: 'https://www.svenshen.com'
+            }]
+        }],
+        sidebar: [{
+            title: '买房调研',
+            path: '/',
+            collapsable: false,
+            children: [{
+                title: "关于",
+                path: "/"
+            }]
+        },
+        {
+            title: "南京",
+            path: '/nanjing/about',
+            collapsable: true,
+            children: [{
+                title: "政策",
+                path: "/nanjing/policy"
+
+            },
+            {
+                title: "计划",
+                path: "/nanjing/plan"
+            },
+            {
+                title: "价格",
+                path: "/nanjing/price"
+            }],
+        },
+        {
+            title: "芜湖",
+            path: '/wuhu/about',
+            collapsable: true,
+            children: [{
+                title: "区域",
+                path: "/wuhu/area"
+            },
+            {
+                title: "政策",
+                path: "/wuhu/policy"
+            },
+            {
+                title: "学区",
+                path: "/wuhu/school"
+            },
+            {
+                title: "计划",
+                path: "/wuhu/plan"
+            },
+            {
+                title: "价格",
+                path: "/wuhu/price"
+            }],
+        },
+        {
+            title: "合肥",
+            path: '/hefei/why',
+            collapsable: true,
+            children: [{
+                title: "计划",
+                path: "/hefei/plan"
+            },
+            {
+                title: "价格",
+                path: "/hefei/price"
+            }],
+        }]
+    }
 }
---
+++ nanjing/policy.md
@@ -0,0 +1,2 @@
+# 政策法规
+- [南京限购政策2023](http://m.nj.bendibao.com/news/104769.shtm)
